f3415bb867e3de4061f2c552cc5191c6612a72d8
Jiaxin Li <jl2726@cornell.edu>
2024-05-02 15:10:11 -0400

    fix cache_stats snoop updates

--- cache_stats.c
+++ cache_stats.c
@@ -55,6 +55,13 @@ void update_stats(cache_stats_t *stats, bool hit_f, bool writeback_f, bool upgra
     if (writeback_f)
     stats->n_writebacks++;
   }
+
+  if (action == 'ST_MISS' || action == 'LD_MISS'){
+    stats->n_bus_snoops++;
+    if (hit_f)
+      stats->n_snoop_hits++;
+  }
+    
 }
 
 // could do this in the previous method, but that's a lot of extra divides...
